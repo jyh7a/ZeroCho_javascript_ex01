a67152eb2cb6f1e6321ec6fa09c5f2fdf15b8a03
jyh7a <40563068+jyh7a@users.noreply.github.com>
2019-07-18 17:54:59 +0900

    자스스톤 업데이트
    https://www.youtube.com/watch?v=NoT2Jm2uycE&list=PLcqDmjxt30Rtbxbh4eJREOVekql_kWVmu&index=92

--- README.md
+++ README.md
@@ -3,8 +3,23 @@
  단축키 홈페이지 : https://demun.github.io/vscode-tutorial/shortcuts/ <br>
  영역접기 ctrl + shift + [ <br>
  영역펼치기 ctrl + shift + ] <br>
+ ctrl+k ctrl+0	모든 영역 접기 <br>
+ ctrl+k ctrl+j	모든 영역 펼치기 <br>
 
- 
+<hr/> 
 12-5<br>
 document.querySelector('.card-hidden .card').cloneNode(true);<br>
 cloneNode로 기존태그를 그대로 복사 가능 인자에 true 넣으면 내부까지 복사<br>
+
+<hr/>
+12-6 
+appendchild 와 append 차이점및 prepend 등에대해 좋은? 자료찾음 왜 prependChild는 없냐;; What the Fork...<br>
+https://rpubs.com/raulUbiqum/append<br>
+
+<hr>
+12-8
+요소.classList.toggle('class명') => 쓸만한 매소드인듯<br>
+document.getElementById('rival').classList.toggle('turn');<br>
+
+<hr>
+
--- js/자스스톤.js
+++ js/자스스톤.js
@@ -1,54 +1,147 @@
-var 상대영웅 = document.getElementById('rival-hero');
-var 내영웅 = document.getElementById('my-hero');
-var 상대덱 = document.getElementById('rival-deck');
-var 내덱 = document.getElementById('my-deck');
-var 상대덱data = [];
-var 내덱data = [];
-var 상대영웅data = [];
-var 내영웅data = [];
+var 상대 = {
+  영웅: document.getElementById('rival-hero'),
+  덱: document.getElementById('rival-deck'),
+  필드: document.getElementById('rival-cards'),
+  코스트: document.getElementById('rival-cost'),
+  덱data: [],
+  영웅data: [],
+  필드data: [],
+}
 
-초기세팅();
+var 나 = {
+  영웅: document.getElementById('my-hero'),
+  덱: document.getElementById('my-deck'),
+  필드: document.getElementById('my-cards'),
+  코스트: document.getElementById('my-cost'),
+  덱data: [],
+  영웅data: [],
+  필드data: [],
+}
 
-function 초기세팅() {
-  상대덱생성(5);
-  내덱생성(5);
-  내영웅생성();
-  상대영웅생성();    
+var 턴표시 = document.getElementById('turn-arrow');
+var 턴버튼 = document.getElementById('turn-btn');
+var 턴 = true; // true면 내턴, false 면 니턴 둘다 아니면 샘헤밍턴~
+
+function 덱에서필드로(데이터, 내턴) {
+  var 객체 = 내턴 ? 나 : 상대;
+  var idx = 객체.덱data.indexOf(데이터);
+  객체.덱data.splice(idx, 1);
+  객체.필드data.push(데이터);
+  객체.덱.innerHTML = '';
+  객체.필드.innerHTML = '';
+  객체.필드data.forEach(function (data) {
+    카드돔연결(data, 객체.필드);
+  });
+  객체.덱data.forEach(function (data) {
+    카드돔연결(data, 객체.덱);
+  });
+  데이터.field = true;
+  객체.코스트.textContent = 현재코스트 - 데이터.cost;
+}
+
+function 카드돔연결(데이터, 돔, 영웅) {
+  var 카드 = document.querySelector('.card-hidden .card').cloneNode(true);
+  카드.querySelector('.card-cost').textContent = 데이터.cost;
+  카드.querySelector('.card-att').textContent = 데이터.att;
+  카드.querySelector('.card-hp').textContent = 데이터.hp;
+  if (영웅) {
+    카드.querySelector('.card-cost').style.display = 'none';
+    var 이름 = document.createElement('div');
+    이름.textContent = '영웅';
+    카드.prepend(이름);
+  }
+  카드.addEventListener('click', function () {
+    if (턴) { // 내 턴이면
+      if (!데이터.mine || 데이터.field) { // 상대 카드면
+        return;
+      }
+      var 현재코스트 = Number(나.코스트.textContent);
+      if (현재코스트 < 데이터.cost) {
+        return;
+      }
+      덱에서필드로(데이터, true);
+      내덱생성(1);
+    } else { // 상대 턴인데
+      if (데이터.mine || 데이터.field) { // 내카드를 누르면
+        return;
+      }
+      var 현재코스트 = Number(상대.코스트.textContent);
+      if (현재코스트 < 데이터.cost) {
+        return;
+      }
+      덱에서필드로(데이터, false);
+      상대덱생성(1);
+    }
+  });
+  돔.appendChild(카드);
 }
 
 function 상대덱생성(개수) {
   for (var i = 0; i < 개수; i++) {
-    상대덱data.push(카드공장());
+    상대.덱data.push(카드공장(false, false));
   }
-  상대덱data.forEach(function(data){
-    var 카드 = document.querySelector('.card-hidden .card').cloneNode(true);
-    상대덱.appendChild();
+  상대.덱.innerHTML = '';
+  상대.덱data.forEach(function (data) {
+    카드돔연결(data, 상대.덱);
   });
 }
 
 function 내덱생성(개수) {
   for (var i = 0; i < 개수; i++) {
-    내덱data.push(카드공장());
+    나.덱data.push(카드공장(false, true));
   }
+  나.덱.innerHTML = '';
+  나.덱data.forEach(function (data) {
+    카드돔연결(data, 나.덱);
+  });
 }
 
 function 내영웅생성() {
-  내영웅data.push(카드공장());
-
+  나.영웅data.push(카드공장(true, true));
+  카드돔연결(나.영웅data[0], 나.영웅, true);
 }
 
 function 상대영웅생성() {
-  상대영웅data.push(카드공장());
+  상대.영웅data.push(카드공장(true, false));
+  카드돔연결(상대.영웅data[0], 상대.영웅, true);
 }
 
-function Card() {
-  this.att = Math.ceil(Math.random() * 5);
-  this.hp = Math.ceil(Math.random() * 5);
-  this.cost = Math.floor((this.att + this.hp) / 2);
+function Card(영웅, 내카드) {
+  if (영웅) {
+    this.att = Math.ceil(Math.random() * 2);
+    this.hp = Math.ceil(Math.random() * 5) + 25;
+    this.hero = true;
+  } else {
+    this.att = Math.ceil(Math.random() * 5);
+    this.hp = Math.ceil(Math.random() * 5);
+    this.cost = Math.floor((this.att + this.hp) / 2);
+  }
+  if (내카드) {
+    this.mine = true;
+  }
 }
 
-function 카드공장() {
-  return new Card();
+function 카드공장(영웅, 내카드) {
+  return new Card(영웅, 내카드);
 }
 
+function 초기세팅() {
+  상대덱생성(5);
+  내덱생성(5);
+  내영웅생성();
+  상대영웅생성();
+}
 
+턴버튼.addEventListener('click', function () {
+  턴 = !턴;
+  턴 === true ? 턴표시.style.top = "65%" : 턴표시.style.top = "20%";
+  if (턴) {
+    나.코스트.textContent = 10;
+  } else {
+    상대.코스트.textContent = 10;
+  }
+  document.getElementById('rival').classList.toggle('turn');
+  document.getElementById('my').classList.toggle('turn');
+});
+
+초기세팅();
